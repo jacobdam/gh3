70460f6a29acea446efca58ca5523ebf95c02f33
Jacob Dam <jacobdam@users.noreply.github.com>
2025-07-12 23:19:35 +0700

    Fix code format

--- lib/main.dart
+++ lib/main.dart
@@ -100,10 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
-            Text(
-              '0',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text('0', style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
       ),
