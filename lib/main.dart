2b54469ebddfce77062089a3f02296f0b50a16b4
Jacob Dam <jacobdam@users.noreply.github.com>
2025-07-25 13:54:29 +0700

    Fix can not go to app after login

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,15 @@ import 'package:gh3/src/init.dart';
 import 'package:gh3/src/screens/app/gh3_app.dart';
 import 'package:gh3/src/screens/app/auth_viewmodel.dart';
 import 'package:gh3/src/routing/route_collection_service.dart';
-import 'package:gh3/src/services/auth_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
 
   // Get services from dependency injection
-  final authService = getIt<AuthService>();
+  final authVM = getIt<AuthViewModel>();
   final routeCollectionService = getIt<RouteCollectionService>();
-
-  // Create ViewModels manually with their dependencies
-  final authVM = AuthViewModel(authService);
-  await authVM.init();
+  authVM.init();
 
   runApp(
     Gh3App(
